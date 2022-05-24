cfa83fb97f5f15211de89eb5f2a352733719236d
jiyong0419 <82662371+jiyong0419@users.noreply.github.com>
2022-05-24 16:02:17 +0900

    #2.3 The Tower of Babel

--- memo2.js
+++ memo2.js
@@ -36,24 +36,41 @@
 
 #2.1 Installing Express
     ㄴ  nodeJS로 자바스크립트에 접근하는 2가지 방법
-        1. 콘솔에 node index.js (비추)
-        2. npm을 이용하여 접근하는 방법 (강추)
+        1.  콘솔에 node index.js (비추)
+        2.  npm을 이용하여 접근하는 방법 (강추)
             - package.json을 연다
             - "scripts":{"dev" : "node index.js"}를 작성한다
             - 콘솔에 npm run dev 
     ㄴ  서버만들기
-        1. npm을 이용하여 설치하고싶은 package의 이름을 쓴다
+        1.  npm을 이용하여 설치하고싶은 package의 이름을 쓴다
             - npm i express
                 >> package-lock 파일과 node_modules폴더가 생성된다
                 >> node_modules폴더에는 express외에도 설치하는 모든 패키지가 저장된다
                 >> npm i 명령어를 하기전에 json파일은 꼭 닫고서 실행한다
-        2. 모든 package.json파일의 "dependencies"는 해당 package가 동작하는데 필요한 패키지,파일들이 적혀있음
+        2.  모든 package.json파일의 "dependencies"는 해당 package가 동작하는데 필요한 패키지,파일들이 적혀있음
                 >> npm i express를 하면 express 패키지의 dependencies도 같이 다운받아진다
 
 #2.2 Understanding Dependencies
     ㄴ  node_modules폴더와 package-lock.json을 지워도 간단히 복구할수있다.
-        - npm i 
-            >> package.json의 "dependencies"를 보고 npm이 그 안의 모듈들을 다시 재설치해준다.
+        1.  npm i 
+            - package.json의 "dependencies"를 보고 npm이 그 안의 모듈들을 다시 재설치해준다.
     ㄴ  .gitigonore 파일을 만들어서 node_modules폴더의 커밋을 무시해준다.
-        - /node_modules
-*/
+        1.  /node_modules
+
+#2.3 The Tower of Babel
+    ㄴ  js파일에서 express 쓰는법 
+        1.  (구문법, 비추하는방법)
+            const express = require("express")
+            const app = express()
+    ㄴ  바벨 설치하기
+        1. babeljs.io
+        2. Setup
+        3. Node
+        4. ②Installation 복사 -> 콘솔에 붙혀넣기
+            >> package.json에 "devDependencies"에 @babel/core가 추가됨
+            >> "devDependencies"는 개발자에게 필요한 Dependencies들
+        5. bael.config.json파일 생성
+        6. ④Create babel.config.json configuration file에서 JSON안에 내용을 복사 -> babel.config.json파일에 붙혀넣기
+        7. ④Create babel.config.json configuration file에서 Shell안에 내용을 복사 -> 콘솔에 붙혀넣기
+            >> package.json에 "devDependencies"에 @babel/preset-env가 추가됨
+        */      
