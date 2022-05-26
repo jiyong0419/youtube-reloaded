a88f964dd86fc1b436788eb00bf934e5f0fe2eb3
jiyong0419 <82662371+jiyong0419@users.noreply.github.com>
2022-05-26 18:50:07 +0900

    #4.0 What are Routers?

---
+++ memo/memo4.js
@@ -0,0 +1,12 @@
+/*
+#4.0 What are Routers?
+    ㄴ  Router는 controller와 url의 관리를 도와주며 카테고리별로 url을 그룹화해준다. (미니어플리케이션)
+        1. / , /login , /join 
+            - 글로벌한 url들
+        2. /users/edit, /users/delete
+            - user관련 url들
+        3. /videos/watch, /videos/edit, /videos/delete, /videos/comments, /videos/comments/delete
+            - video관련 url들
+
+    ㄴ  README파일에 필요한 url들을 정리해보도록 하자
+*/
