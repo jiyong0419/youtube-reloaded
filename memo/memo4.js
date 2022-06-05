e3aa516333ae7614d8bcec6b47bf0410097cfc1a
jiyong0419 <82662371+jiyong0419@users.noreply.github.com>
2022-06-05 22:25:11 +0900

    #4.2 Cleaning the Code

--- memo/memo4.js
+++ memo/memo4.js
@@ -28,7 +28,39 @@
            userRouter.get("/",handleEditUser)
            videoRouter.get("/",handleWatchVideo)
             - 각각 Router.get으로 각각 Router들에게 controller를 적용한다.
-        
+            
+#4.2 Cleaning the Code
+    ㄴ  Router를 따로 모아두는 폴더를 만들자
+        1. src안에 routers폴더를 생성
+        2. routers폴더안에 globalRouter.js / userRouter.js / videoRouter.js 생성
+        3. 각 Router.js에 Router선언문을 집어넣는다.
+            - import express from "express"
+              const globalRouter = express.Router() 
+            - import express from "express"
+              const userRouter = express.Router() 
+            - import express from "express"
+              const videoRouter = express.Router() 
+        4. 각 Router.js에 Router.get()을 집어넣는다.
+            - globalRouter.get("/",handleHome)
+            - globalRouter.get("/edit",handleEditUser)
+            - globalRouter.get("/watch",handleWatchVideo)
+        5. 각 Router.js에 controller를 집어넣는다.
+            - globalRouter.get("/",handleHome)
+            - userRouter.get("/edit",handleEditUser)
+            - videoRouter.get("/watch",handleWatchVideo)
+        5. 각 Router.js에서 Router변수들을 export해주자
+            - export default globalRouter
+            - export default userRouter
+            - export default videoRouter
+        5. 메인 JS파일에서 각 Router변수들을 import해주자
+            - import globalRouter from ""
+              import userRouter from ""
+              import videoRouter from ""
+    
+    ㄴ  export default는 변수하나를 export한다
+        1. 자세한 내용은 #4.3강의에서
+
+
         
         
         
