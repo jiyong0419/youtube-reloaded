5d60f09ba81694c1e1ccb64a197eb48dd124d5e7
jiyong0419 <82662371+jiyong0419@users.noreply.github.com>
2022-06-06 09:23:45 +0900

    #5.0 Returning HTML

---
+++ memo/memo5.js
@@ -0,0 +1,6 @@
+/*
+#5.0 Returning HTML
+    ㄴ controller에서 res.send("Home")같이 문자열을 리턴하지않고 HTML을 리턴할것이다
+        1. res.send("<h1>Homepage!</h1>") >>> ""안에 html을 작성하는 방법이 있다.
+        2. PUG를 이용하는 방법이있다.
+*/
